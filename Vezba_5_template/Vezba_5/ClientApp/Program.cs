c802928e0b3cdc03f0a91e7e13574a0789b174de
Panda-cry <80039775+Panda-cry@users.noreply.github.com>
2021-12-03 19:45:01 +0100

    Stuff

--- Vezba_5_template/Vezba_5/ClientApp/Program.cs
+++ Vezba_5_template/Vezba_5/ClientApp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ServiceModel;
-using Contracts;
+using Contract;
 
 namespace ClientApp
 {
@@ -16,23 +16,7 @@ namespace ClientApp
 
             using (WCFClient proxy = new WCFClient(binding, new EndpointAddress(new Uri(address))))
             {
-                Car car = proxy.Read(1);
-                Console.WriteLine(car);
-
-                car = new Car(Brand.Audi, "Q3", 2014, 68);
-                proxy.Modify(1, car);
-
-                car = proxy.Read(1);
-                Console.WriteLine(car);
-
-                proxy.Delete(1);
-                car = proxy.Read(1);
-                Console.WriteLine(car);
-
-                proxy.ManagePermission(true, "Modifier", "one" );
-                proxy.ManagePermission(false, "Modifier", "one" ); ;
-                proxy.ManageRoles(true, "None");
-                proxy.ManageRoles(false, "None");
+				string aa = proxy.Connect();
             }
 
 			Console.ReadLine();
--- Vezba_5_template/Vezba_5/ClientApp/WCFClient.cs
+++ Vezba_5_template/Vezba_5/ClientApp/WCFClient.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ServiceModel;
-using Contracts;
+using Contract;
 
 namespace ClientApp
 {
-	public class WCFClient : ChannelFactory<IWCFService>, IWCFService, IDisposable
+	public class WCFClient : ChannelFactory<IServiceManager>, IServiceManager, IDisposable
 	{
-		IWCFService factory;
+		IServiceManager factory;
 
 		public WCFClient(NetTcpBinding binding, EndpointAddress address)
 			: base(binding, address)
@@ -17,104 +17,7 @@ namespace ClientApp
 			factory = this.CreateChannel();
 		}
 
-        #region Read()
-
-        public Car Read(int key)
-        {
-            Car car = null;
-            try
-            {
-                car = factory.Read(key);
-                Console.WriteLine("Read allowed");
-            }
-            catch (FaultException<SecurityException> e)
-            {
-                Console.WriteLine("Error while trying to Read : {0}", e.Detail.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error while trying to Read : {0}", e.Message);
-            }
-            return car;
-
-        }
-
-        #endregion
-
-        #region Modify()
-
-        public bool Modify(int key, Car car)
-        {
-            bool retValue = false;
-            try
-            {
-                retValue = factory.Modify(key, car);
-                Console.WriteLine("Modify allowed");
-            }
-            catch (FaultException<SecurityException> e)
-            {
-                Console.WriteLine("Error while trying to Modify : {0}", e.Detail.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error while trying to Modify : {0}", e.Message);
-            }
-            return retValue;
-        }
-
-
-        #endregion
-
-        #region Delete()
-        public bool Delete(int key)
-        {
-            bool retValue = false;
-            try
-            {
-                retValue = factory.Delete(key);
-                Console.WriteLine("Delete allowed");
-            }
-            catch (FaultException<SecurityException> e)
-            {
-                Console.WriteLine("Error while trying to Delete : {0}", e.Detail.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error while trying to Delete : {0}", e.Message);
-            }
-            return retValue;
-        }
-
-        #endregion
-        #region ManagePrms
-        
-        public void ManagePermission(bool isAdd, string rolename, params string[] permissions)
-        {
-            try
-            {
-                factory.ManagePermission(isAdd, rolename, permissions);
-                Console.WriteLine("Manage allowed");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error while trying to Manage : {0}", e.Message);
-            }
-        }
-
-        public void ManageRoles(bool isAdd, string rolename)
-        {
-            try
-            {
-                factory.ManageRoles(isAdd, rolename);
-                Console.WriteLine("Manage allowed");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error while trying to Manage : {0}", e.Message);
-            }
-        }
-
-        #endregion
+      
         public void Dispose()
 		{
 			if (factory != null)
@@ -125,6 +28,14 @@ namespace ClientApp
 			this.Close();
 		}
 
-       
+        public string Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool StartNewService(string encryptedMessage)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
